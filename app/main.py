f26dcddf093ea15c1f1f3f3c712f9f9a8462c13f
Glas42 <145870870+Glas42@users.noreply.github.com>
2024-07-12 20:18:19 +0200

    updater fix

--- app/main.py
+++ app/main.py
@@ -26,12 +26,11 @@ settings.Get("Draw", "MouseSlowdown", 1)
 def update_check():
     try:
         remote_version = requests.get("https://raw.githubusercontent.com/Glas42/PyTorch-Calculator/main/version.txt").text.strip()
-        changelog = requests.get("https://raw.githubusercontent.com/Glas42/PyTorch-Calculator/main/changelog.txt").text.strip()
     except:
         print(f"{variables.RED}Failed to check for updates:{variables.NORMAL}\n" + str(traceback.format_exc()))
     if remote_version != variables.VERSION and settings.Get("Update", "AutoUpdate", True):
         try:
-            print(f"New version available: {remote_version}\nChangelog:\n{changelog}")
+            print(f"New version available: {remote_version}")
             os.chdir(variables.PATH)
             os.system("git stash")
             os.system("git pull")
