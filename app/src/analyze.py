28767db86b57f8055c9f1876f73da281128e098b
Glas42 <145870870+Glas42@users.noreply.github.com>
2024-10-14 21:30:05 +0200

    the analyze.py now draws the combinations

--- app/src/analyze.py
+++ app/src/analyze.py
@@ -21,6 +21,7 @@ UPDATING = False
 def Initialize():
     global LastContent
     global EmptyFrame
+    global BaseImage
     global Frame
 
     if variables.DEVMODE:
@@ -28,6 +29,7 @@ def Initialize():
 
     LastContent = None
     EmptyFrame = numpy.zeros((500, 500, 3), numpy.uint8)
+    BaseImage = numpy.zeros((50, 50, 3), numpy.uint8)
     Frame = EmptyFrame.copy()
 
 
@@ -40,6 +42,7 @@ def Update():
                     time.sleep(0.1)
                 UPDATING = True
                 global LastContent
+                global EmptyFrame
                 global Frame
 
                 Content = (len(variables.CANVAS_CONTENT))
@@ -49,12 +52,14 @@ def Update():
                         Frame = EmptyFrame.copy()
                     CANVAS_CONTENT = variables.CANVAS_CONTENT
 
+                    Start = time.perf_counter()
+
                     print(PURPLE + "Analyzing content..." + NORMAL)
                     print(GRAY + f"-> Lines: {len(CANVAS_CONTENT)}" + NORMAL)
                     print(GRAY + f"-> Points: {sum([len(line) if len(line[0]) != 4 else len(line[1:]) for line in CANVAS_CONTENT])}" + NORMAL)
 
                     print(BLUE + "Generating combinations..." + NORMAL)
-                    Combinations = [line[1:] for line in CANVAS_CONTENT]
+                    Combinations = [[line[1:]] for line in CANVAS_CONTENT]
                     for r in range(2, len(CANVAS_CONTENT) + 1):
                         for Combination in itertools.combinations(CANVAS_CONTENT, r):
                             CombinedLines = []
@@ -68,9 +73,36 @@ def Update():
 
                     print(BLUE + "Interpreting combinations..." + NORMAL)
 
-                    print("NOT IMPLEMENTED: Render and check all combinations using a classification model.")
+                    for Combination in Combinations:
+                        Image = BaseImage.copy()
+                        LastLen = 0
+                        MinX = min([Point[0] for Line in Combination for Point in Line])
+                        MinY = min([Point[1] for Line in Combination for Point in Line])
+                        MaxX = max([Point[0] for Line in Combination for Point in Line])
+                        MaxY = max([Point[1] for Line in Combination for Point in Line])
+                        ScaleX = ((Image.shape[1] - 1) / (MaxX - MinX)) if MaxX - MinX != 0 else 1e9
+                        ScaleY = ((Image.shape[0] - 1)  / (MaxY - MinY)) if MaxY - MinY != 0 else 1e9
+                        Scale = min(ScaleX, ScaleY)
+                        XOffset = ((Image.shape[1] - 1) - (MaxX - MinX) * Scale) / 2
+                        YOffset = ((Image.shape[0] - 1) - (MaxY - MinY) * Scale) / 2
+                        for Line in Combination:
+                            LastPoint = None
+                            for Point in Line:
+                                if LastPoint != None:
+                                    cv2.line(Image, (round((LastPoint[0] - MinX) * Scale + XOffset), round((LastPoint[1] - MinY) * Scale + YOffset)), (round((Point[0] - MinX) * Scale + XOffset), round((Point[1] - MinY) * Scale + YOffset)), (255, 255, 255), 1)
+                                elif len(Line) == 1:
+                                    cv2.line(Image, (round((Point[0] - MinX) * Scale + XOffset), round((Point[1] - MinY) * Scale + YOffset)), (round((Point[0] - MinX) * Scale + XOffset), round((Point[1] - MinY) * Scale + YOffset)), (255, 255, 255), 1)
+                                LastPoint = Point
+                        if len(Combination) > LastLen:
+                            LastLen = len(Combination)
+                            EmptyFrame = cv2.resize(Image, (500, 500))
+                            Frame = EmptyFrame.copy()
 
-                    print(PURPLE + "Done!\n" + NORMAL)
+                    print("NOT IMPLEMENTED: Check all combinations using a classification model.")
+
+                    print(PURPLE + f"Analyzing completed!" + NORMAL)
+                    print(GRAY + f"-> {round((time.perf_counter() - Start), 2)}s" + NORMAL)
+                    print()
 
                     LastContent = Content
 
